a1e037e3bb1fa2d44a08d59ec1270ab079e2b645
YuriyTikhonoff <yuryitikhonoff@gmail.com>
2020-08-20 10:19:35 +0300

    Make reguest to JSONplaceholder

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 
-setTimeout(() => alert('Hello world'), 3000)
+const Url = 'https://jsonplaceholder.typicode.com/posts?_limit=5'
+
+fetch(Url)
+
+.then(data => {return data.json()})
+.then(res => {console.log(res)})
+
+
+function displayPost(post) {
+    let title = post.title
+    let body = post.body
+
+}
